fe809731fee876f82a7d366c4542238cd63540f2
ahamed.zoha@gmail.com <ahamed.zoha@gmail.com>
2020-02-19 23:25:14 +0600

    Updates

---
+++ precache-manifest.8607159af5d92afa5d8cc1c6dc61a4e2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4e7c2b50f7c0c1e7fc117eec54ddd3e",
+    "url": "/practice-react-app-01/index.html"
+  },
+  {
+    "revision": "3e68b60613a7e97cbc79",
+    "url": "/practice-react-app-01/static/css/main.dfc90c13.chunk.css"
+  },
+  {
+    "revision": "578b5983e31339b26b19",
+    "url": "/practice-react-app-01/static/js/2.02d23424.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/practice-react-app-01/static/js/2.02d23424.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e68b60613a7e97cbc79",
+    "url": "/practice-react-app-01/static/js/main.bd691aa6.chunk.js"
+  },
+  {
+    "revision": "dbc544d600db657409ee",
+    "url": "/practice-react-app-01/static/js/runtime-main.34736d40.js"
+  }
+]);
